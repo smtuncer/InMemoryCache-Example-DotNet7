2ee94bfa5862f1271a01ea5737137880f7f7b388
Selçuk TUNÇER <smtuncer123@hotmail.com>
2023-08-27 10:44:22 +0300

    Add project files.

---
+++ InMemory/Controllers/BlogController.cs
@@ -0,0 +1,44 @@
+﻿using InMemory.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace InMemory.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BlogController : ControllerBase
+    {
+        private readonly IMemoryCache _cache;
+
+        public BlogController(IMemoryCache cache)
+        {
+            _cache = cache;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<BlogPost> GetBlogPost(int id)
+        {
+            BlogPost blogPost;
+            if (!_cache.TryGetValue(id, out blogPost))
+            {
+                blogPost = new BlogPost
+                {
+                    Id = id,
+                    Title = "Test Başlığı",
+                    Content = "Test İçeriği"
+                };
+
+                var cacheEntryOptions = new MemoryCacheEntryOptions
+                {
+                    AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(5)
+                };
+
+                _cache.Set(id, blogPost, cacheEntryOptions);
+
+                Console.WriteLine("Önbelleğe alındı");
+            }
+
+            return blogPost;
+        }
+    }
+}
---
+++ InMemory/Model/BlogPost.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InMemory.Model
+{
+    public class BlogPost
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+    }
+}
